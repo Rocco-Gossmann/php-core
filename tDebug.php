afbbb906d4fd901c1cf3225507fa60d9a72fa4cf
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2025-11-13 07:04:36 +0100

    fix: lambdas are more expensive, than this if check

--- tDebug.php
+++ tDebug.php
@@ -2,34 +2,24 @@
 
 trait tDebug {
 
-	private static $__debugToError = __CLASS__ . "::_debugNoop";
-	private static $__debugToEcho = __CLASS__ . "::_debugNoop";
+	private static $__debugDisbaled = false;
 
 	public static function setDebug($mode) {
 
-		if(empty($mode))  {
-			self::$__debugToEcho = __CLASS__ . "::_debugNoop";
-			self::$__debugToError = __CLASS__ . "::_debugNoop";
-		}
-		else {
-			self::$__debugToEcho = __CLASS__ . "::_toPrint";
-			self::$__debugToError = __CLASS__ . "::_toErrorLog";
-		}
+		self::$__debugDisbaled = empty($mode);
 
 	}
 
-
-	private static function toErrorLog(...$args) { (self::$__debugToError)($args); }
-	private static function toPrint(...$args) { (self::$__debugToEcho)($args); }
-
-	private static function _debugNoop() {}
-	private static function _toErrorLog($args) {
+	protected static function toErrorLog(...$args) {
+		if(self::$__debugDisbaled) return;
 		$sPrefix = "[" . __CLASS__ . "]";
 		foreach($args as $arg) {
 			error_log($sPrefix . " => " . print_r($arg, true));
 		}
 	}
-	private static function _toPrint($args) {
+
+	protected static function toPrint(...$args) {
+		if(self::$__debugDisbaled) return;
 		echo "[" . __CLASS__ . "]";
 		foreach($args as $arg)
 			echo "\t => " , var_dump($arg, true), "\n";
