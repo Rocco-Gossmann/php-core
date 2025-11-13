17b099000cfd4eca61f3b4dbefa4ff9aa0982850
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2025-11-13 05:54:37 +0100

    refactor: more performant DebugOutput
    (no more if checks)

--- tDebug.php
+++ tDebug.php
@@ -2,26 +2,36 @@
 
 trait tDebug {
 
-	private static $__debugmode = false;
+	private static $__debugToError = __CLASS__ . "::_debugNoop";
+	private static $__debugToEcho = __CLASS__ . "::_debugNoop";
 
 	public static function setDebug($mode) {
-		self::$__debugmode = $mode != false;
+
+		if(empty($mode))  {
+			self::$__debugToEcho = __CLASS__ . "::_debugNoop";
+			self::$__debugToError = __CLASS__ . "::_debugNoop";
+		}
+		else {
+			self::$__debugToEcho = __CLASS__ . "::_toPrint";
+			self::$__debugToError = __CLASS__ . "::_toErrorLog";
+		}
+
 	}
 
-	public static function toErrorLog(...$args) {
-		if(self::$__debugmode == false) return;
 
+	private static function toErrorLog(...$args) { (self::$__debugToError)($args); }
+	private static function toPrint(...$args) { (self::$__debugToEcho)($args); }
+
+	private static function _debugNoop() {}
+	private static function _toErrorLog($args) {
 		$sPrefix = "[" . __CLASS__ . "]";
 		foreach($args as $arg) {
 			error_log($sPrefix . " => " . print_r($arg, true));
 		}
 	}
-
-	public static function toPrint(...$args) {
-		if(self::$__debugmode == false) return;
-
+	private static function _toPrint($args) {
 		echo "[" . __CLASS__ . "]";
-		foreach($args as $arg) 
+		foreach($args as $arg)
 			echo "\t => " , var_dump($arg, true), "\n";
 	}
 }
